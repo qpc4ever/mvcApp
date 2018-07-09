e100961e8887ef0bce66c5762db135fbfe5b1405
qpc4ever <qpc4ever@gmail.com>
2018-07-09 13:55:30 -0400

    Contact feature complete

--- mvcApp/Controllers/HomeController.cs
+++ mvcApp/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mvcApp.Models;
+
 
 namespace mvcApp.Controllers
 {
@@ -27,11 +29,53 @@ namespace mvcApp.Controllers
             ViewData["Message"] = "Your contact page.";
             var contact = new ContactViewModel();
 
-            return View();
+            return View(contact);
         }
         [HttpPost]
         public IActionResult Contact(ContactViewModel viewModel)
         {
+            if (ModelState.IsValid)
+            {
+                //do something
+                var message = new MailMessage();
+                message.From =
+                    new MailAddress("do_not_reply@sightsource.net",
+                        "Sightsource Contact Bot");
+
+                //subject
+
+                message.Subject = "New contact message";
+                
+                //To
+
+                message.To.Add(new MailAddress("qpc4ever@gmail.com"));
+
+                //message
+
+                message.Body = $"new contact from {viewModel.Name} ({viewModel.Email})" + 
+                    Environment.NewLine + 
+                    viewModel.Message;
+
+                var mailClient = new SmtpClient("email-smpt.us-east-1.amazonaws.com", 587);
+                
+
+                mailClient.UseDefaultCredentials = false;
+                mailClient.Credentials = new System.Net.NetworkCredential(
+                    "AKIAJZT46OGOXRVDV55Q",
+                    "AnQ9XTmy2Bb7g+adRah8ZLVkJzvwQr3y448eeVfqfGg"
+                    );
+
+                mailClient.EnableSsl = true;
+                try
+                {
+                    //mailClient.Send(message);
+                    viewModel.CompletedAt = DateTime.UtcNow;
+                }
+                catch(Exception ex)
+                {
+                    viewModel.ErrorMessage = "Ooops. Something went wrong";
+                }
+            }
             return View(viewModel);
         }
 
--- mvcApp/Models/ContactViewModel.cs
+++ mvcApp/Models/ContactViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,23 @@ namespace mvcApp.Models
 {
     public class ContactViewModel
     {
+        [Required(ErrorMessage="Please enter your name")]
+        [MaxLength(200, ErrorMessage = "Name cannot exceed 200 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email address")]
+        [MaxLength(200, ErrorMessage = "Email cannot exceed 200 characters")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your message")]
+        [MaxLength(4000, ErrorMessage = "Message cannot exceed 4000 characters")]
         public string Message { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+
+        public bool HasErrored => !string.IsNullOrWhiteSpace(ErrorMessage);
+      
+        public string ErrorMessage { get; set; }
     }
 }
