4c72fc275aaf82554f2cc0d18604695a26bbb24a
qpc4ever <qpc4ever@gmail.com>
2018-07-10 09:03:51 -0400

    Day 2 push

---
+++ mvcApp/Controllers/NAvController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvcApp.Controllers
+{
+    public class NAvController : Controller
+    {
+        public IActionResult TopMenu()
+        {
+            return View();
+        }
+    }
+}
